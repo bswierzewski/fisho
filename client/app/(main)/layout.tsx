78e2f9276307124c839956d255934ad2d21d4e56
bswierzewski <swierzewski.bartosz@gmail.com>
2025-05-06 09:29:17 +0200

    Add sidebar

--- client/app/(main)/layout.tsx
+++ client/app/(main)/layout.tsx
@@ -1,12 +1,12 @@
-import { BottomNavbar } from '@/components/layout/BottomNavbar';
 import Navbar from '@/components/layout/Navbar';
+import { Sidebar } from '@/components/shared/Sidebar';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <Sidebar />
       <Navbar />
       <main className="container mx-auto px-4 pt-4 pb-20 flex-grow">{children}</main>
-      <BottomNavbar />
     </div>
   );
 }
--- client/components/layout/Navbar.tsx
+++ client/components/layout/Navbar.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { useSidebarStore } from '@/lib/store/sidebarStore';
 import { UserButton } from '@clerk/nextjs';
+import { Menu } from 'lucide-react';
 
 export default function Navbar() {
+  const { setOpen } = useSidebarStore();
   return (
-    <nav className="p-4 z-50">
+    <nav className="p-4 z-50 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
+        <Menu onClick={() => setOpen(true)} />
+
         <span>Strona główna</span>
 
-        <div className="flex items-center">
-          <UserButton />
-        </div>
+        <UserButton />
       </div>
     </nav>
   );
