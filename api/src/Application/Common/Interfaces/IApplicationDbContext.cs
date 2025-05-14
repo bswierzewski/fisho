2a5017c79a3d7fbc8eaff746b1aa570ba89e97df
bswierzewski <swierzewski.bartosz@gmail.com>
2025-05-14 22:40:25 +0200

    Add db sets, remove and sort using

--- api/src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ api/src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -2,11 +2,15 @@
 
 public interface IApplicationDbContext
 {
-    DbSet<LogbookEntry> LogbookEntries { get; }
-    DbSet<Fishery> Fisheries { get; }
-    DbSet<FishSpecies> FishSpecies { get; }
     DbSet<User> Users { get; }
     DbSet<Competition> Competitions { get; }
+    DbSet<CompetitionParticipant> CompetitionParticipants { get; }
+    DbSet<CompetitionFishCatch> CompetitionFishCatches { get; }
+    DbSet<Fishery> Fisheries { get; }
+    DbSet<FishSpecies> FishSpecies { get; }
+    DbSet<LogbookEntry> LogbookEntries { get; }
+    DbSet<ScoringCategoryOption> ScoringCategoryOptions { get; }
+    DbSet<SpecialCompetitionCategory> SpecialCompetitionCategories { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
