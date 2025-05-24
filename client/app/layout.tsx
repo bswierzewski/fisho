52d2f5da470ec45975975f350e54009362ae998e
bswierzewski <swierzewski.bartosz@gmail.com>
2025-05-24 23:31:48 +0200

    Set toaster position to bottom-right

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           <AxiosClientConfigurator />
           <TanstackQueryProvider>
             {children}
-            <Toaster />
+            <Toaster position="bottom-right" />
           </TanstackQueryProvider>
         </body>
       </html>
