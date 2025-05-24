c49e83e397d140ec760c165607453755898c75c6
bswierzewski <swierzewski.bartosz@gmail.com>
2025-05-24 20:31:24 +0200

    Add swagger jwt token

--- api/src/API/DependencyInjection.cs
+++ api/src/API/DependencyInjection.cs
@@ -26,6 +26,31 @@ public static class DependencyInjection
         {
             options.SwaggerDoc("v1", new OpenApiInfo { Title = "Fishio API", Version = "v1" });
             options.SchemaFilter<EnumSchemaFilter>();
+
+            // Add JWT Bearer security definition
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
         });
 
         services.ConfigureHttpJsonOptions(services =>
